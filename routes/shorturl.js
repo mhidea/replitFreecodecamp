c3b342645c1d63a8aeae02a89dc24194946dd5ea
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2021-08-15 12:14:31 +0430

    validate

--- routes/shorturl.js
+++ routes/shorturl.js
@@ -15,40 +15,37 @@ function validURL(str) {
 
 app.post("/api/shorturl/:shorturl?", function (req, res) {
     if (req.params.shorturl != undefined) {
-        if (validURL(req.params.shorturl)) {
-            urlModel.findOne({ _id: req.params.shorturl }, function (err, doc) {
-                if (err || !doc) {
-                    return res.json({ error: 'invalid url' })
-                } else {
-                    res.redirect(doc.url)
-                }
-            });
-        } else {
-            return res.json({ error: 'invalid url' })
-
-        }
-
-    } else {
-        dns.lookup(req.body.URL, function (err, address, family) {
-            if (err) {
+        urlModel.findOne({ _id: req.params.shorturl }, function (err, doc) {
+            if (err || !doc) {
                 return res.json({ error: 'invalid url' })
             } else {
-                urlModel.findOne({ url: req.body.URL }, function (err, doc) {
-                    if (err || !doc) {
-                        urlModel.create({ url: req.body.URL }, function (err, model) {
-                            if (err) {
-                                console.log('model not saved');
-                            } else {
-                                return res.json({ original_url: req.body.URL, short_url: model._id })
-                            }
-                        });
-                    } else {
-                        return res.json({ original_url: req.body.URL, short_url: doc._id })
-                    }
-                });
+                res.redirect(doc.url)
             }
-        })
-
+        });
+    } else {
+        if (validURL(req.body.URL)) {
+            dns.lookup(req.body.URL, function (err, address, family) {
+                if (err) {
+                    return res.json({ error: 'invalid url' })
+                } else {
+                    urlModel.findOne({ url: req.body.URL }, function (err, doc) {
+                        if (err || !doc) {
+                            urlModel.create({ url: req.body.URL }, function (err, model) {
+                                if (err) {
+                                    console.log('model not saved');
+                                } else {
+                                    return res.json({ original_url: req.body.URL, short_url: model._id })
+                                }
+                            });
+                        } else {
+                            return res.json({ original_url: req.body.URL, short_url: doc._id })
+                        }
+                    });
+                }
+            })
+        } else {
+            return res.json({ error: 'invalid url' })
+        }
     }
 
 

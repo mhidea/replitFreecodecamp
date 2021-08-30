72d203689f335e75092b44ce7b61cc4f992c2f92
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2021-08-30 12:29:40 +0430

    metadata

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,14 @@
 
 const { app } = require('../app')
-
+const multer = require('multer')
+const upload = multer({ dest: '../public/uploads/' })
 
 app.get("/", function (req, res) {
-    return res.sendFile(__dirname.replace('routes', '') + "views/index.html")
+    return res.sendFile(__dirname.replace('routes', '') + "views/metaform.html")
+})
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+    return res.json({ name: req.file.originalname, size: req.file.size, type: req.file.mimetype })
 })
 
 
